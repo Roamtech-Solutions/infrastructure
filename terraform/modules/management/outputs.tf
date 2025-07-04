27e89621fdc2d0c8a14c1d6700cc4d04c74f2657
bob-crutchley <bob.crutchley@whitemire-technologies.com>
2025-07-04 15:15:30 +0100

    initial gitlab ci

--- terraform/modules/management/outputs.tf
+++ terraform/modules/management/outputs.tf
@@ -2,3 +2,7 @@ output "workload_identity_pool_provider_name" {
    value = module.gitlab_oidc.workload_identity_pool_provider_name
 }
 
+output "gitlab_ci_sa_email" {
+	value = google_service_account.gitlab_ci.email
+}
+
