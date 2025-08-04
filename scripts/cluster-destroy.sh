bfaca093c25ef266597bb9ec8cd91e3059ccd950
Bob Crutchley <bob.crutchley@whitemire-technologies.com>
2025-08-04 06:51:00 +0100

    Removing scripts

--- scripts/cluster-destroy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-for release in $(helm list --all-namespaces --no-headers | awk '{ print $1":"$2 }'); do
-	name=$(echo ${release} | cut -d ':' -f 1)
-	namespace=$(echo ${release} | cut -d ':' -f 2)
-	echo "$name -> ${namespace}"
-done
-
