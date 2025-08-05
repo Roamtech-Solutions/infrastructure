b560da0c8320b6afdc71bf1330898c0a5df8c706
Bob Crutchley <bob.crutchley@whitemire-technologies.com>
2025-08-05 08:01:49 +0100

    implementing secrets for services

--- terraform/modules/application-service/main.tf
+++ terraform/modules/application-service/main.tf
@@ -31,57 +31,12 @@ resource "google_dns_record_set" "default" {
 }
 
 /* === Secrets === */
-# resource "google_secret_manager_secret" "empty" {
-# 	for_each = local.secrets_empty
-# 	project = var.project_id
-# 	secret_id = each.key
-#   replication {
-#     auto {}
-#   }
-# }
-# 
-# resource "google_secret_manager_secret" "generated" {
-# 	for_each = local.secrets_generated
-# 	project = var.project_id
-# 	secret_id = each.key
-#   replication {
-#     auto {}
-#   }
-# }
-# 
-# resource "random_password" "generated" {
-# 	for_each = local.secrets_generated
-#   length           = lookup(each.value, "length", 16)
-#   special          = lookup(each.value, "special", true)
-#   override_special = "!#$%&*()-_=+[]{}<>:?"
-# }
-# 
-# resource "google_secret_manager_secret_version" "generated" {
-# 	for_each = google_secret_manager_secret.generated
-#   secret      = each.value.id
-#   secret_data = random_password.generated[each.key]
-# }
-# 
-# /* MySQL root password */
-# resource "random_password" "mysql_root" {
-# 	count = (local.mysql) ? 1 : 0
-#   length           = 16
-#   special          = true
-#   override_special = "!#$%&*()-_=+[]{}<>:?"
-# }
-# 
-# resource "google_secret_manager_secret" "mysql_root" {
-# 	count = (local.mysql) ? 1 : 0
-#   project   = var.project_id
-#   secret_id = "${var.name}-mysql-root-password"
-#   replication {
-#     auto {}
-#   }
-# }
-# 
-# resource "google_secret_manager_secret_version" "mysql_root" {
-# 	count = (local.mysql) ? 1 : 0
-#   secret      = google_secret_manager_secret.mysql_root[0].id
-#   secret_data = random_password.mysql_root[0].result
-# }
+resource "google_secret_manager_secret" "default" {
+	for_each = toset(local.secrets)
+	project = var.project_id
+	secret_id = "${var.service_group}-${lower(replace(each.key, "_", "-"))}"
+  replication {
+    auto {}
+  }
+}
 
