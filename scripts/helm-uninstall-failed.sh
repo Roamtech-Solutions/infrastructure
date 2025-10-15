4dddbdb9f47b8684fb3b1f68660d8ae1ff7f8938
Bob Crutchley <bob.crutchley@whitemire-technologies.com>
2025-10-15 08:50:28 +0100

    uninstall failed services job

--- scripts/helm-uninstall-failed.sh
+++ scripts/helm-uninstall-failed.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 
 sg=$1
-if [ -n "${sg}" ]; then
-	helm list -n ${sg} | grep failed | cut -f 1 | xargs helm uninstall -n ${sg}
-else 
-	helm list | grep failed | cut -f 1 | xargs helm uninstall
+if [ -z "${sg}" ]; then
+	echo "Service group not provided."
+	echo "Provide a service group to uninstall failed services from."
+	echo "Otherwise, use 'all', to remove all of the failed services in the environment."
 fi
+
+test "${sg}" = "all" && ns_args="" || ns_args="-n ${sg}"
+helm list ${ns_args} | grep failed | cut -f 1 | xargs helm uninstall ${ns_args}
 
