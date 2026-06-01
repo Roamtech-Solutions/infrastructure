d177fed2fcbc49b7b5a6aaf15ce7141e20b4e36b
Bob Crutchley <bob.crutchley@whitemire-technologies.com>
2026-06-01 09:04:09 +0100

    script updates

--- scripts/repo-setup-workflow-file.sh
+++ scripts/repo-setup-workflow-file.sh
@@ -1,16 +1,48 @@
 #! /bin/sh
-# Install a workflow file in all repos to the default branch
-
+# =========================================================================== #
+#
+# DESCRIPTION:
+#	Install a given workflow file in all provided repos to the default
+#	branch.
+#	GitHub organisation and host is assumed based on ./scripts/common.sh
+#
+# SYNOPSIS:
+#	./scripts/repo-setup-workflow-file.sh FILE REPOSITORIES
+#	
+# EXAMPLES:
+#	Install a local /tmp/sandbox.yaml file to
+#	.github/workflows/sandbox.yaml in the paykit-api repository:
+#
+#	./scripts/repo-setup-workflow-file.sh /tmp/sandbox.yaml paykit-api
+#
+#
+#	Install a workflow file for all repositories in the paykit service
+#	group:
+#
+#	./scripts/repo-setup-workflow-file.sh /tmp/sandbox.yaml \
+#		$(./scripts/github-repo-search.sh paykit)
+#	
+# =========================================================================== #
 . scripts/common.sh
 
 setup_workflow() {
+	# Make sure repository has been provided so we don't delete /tmp
+	if [ -z "${1}" ]; then
+		echo "No repository provided"
+		exit 1
+	fi
+	rm -rf /tmp/${1}
 	git clone $(github_ssh_url ${1}) /tmp/${1}
 	cd /tmp/${1}
 	mkdir -p .github/workflows
 	cp ${WORKFLOW_FILE} .github/workflows/
 	git add .
-	git commit -m "add $(basename ${WORKFLOW_FILE})"
-	git push -u origin $(git branch --show-current)
+	# Check in the new file, if it wasn't already there.
+	if git commit -m "add $(basename ${WORKFLOW_FILE})"; then
+		git push -u origin $(git branch --show-current)
+	else
+		echo "Assuming file already exists..."
+	fi
 	cd -
 	rm -rf /tmp/${1}
 }
